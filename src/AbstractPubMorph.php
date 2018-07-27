c8e0eaaafd795545e6b27d9be88eeaed6e532064
barantaran <yourchev@gmail.com>
2018-07-27 17:43:02 +0300

    Modifies methods in a more specific way
    There is no need to duplicate DOMXML node manipulating abstraction.
    Morpher methods should be implemented in a more specific way for publication morph.

--- src/AbstractPubMorph.php
+++ src/AbstractPubMorph.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Morphes publication text by predefined set of rules.
+ * E.g. remove node from publication:
+ *
+ * $publication = "<div>Some text. <div incut-id='42'>Incut text.</div></div>";
+ * $incutRemoved = \MIR24\PubMorph\Morpher($publication)->removeIncut(42);
+ * */
 namespace \MIR24\PubMorph;
 
 abstract class AbstractPubMorph 
@@ -9,7 +16,16 @@ abstract class AbstractPubMorph
         $this->pubSource = $publicationSource;
         return $this;
     }
-    
-    abstract public function removeNode();
-    abstract public function fillupNode();
+
+    /*
+     * Search for DOM node inside pub text by attribute name and attribute value,
+     * removes node from publication, than returns publication text morphed.
+     * */ 
+    abstract public function removeIncut($nodeAttrValue)
+
+    /*
+     * Search for DOM node inside pub text by attribute name and attribute value,
+     * fillup node with a specific content, than returns publication text morphed.
+     * */ 
+    abstract public function placeIncut(int $nodeAttrValue, string $content);
 }
