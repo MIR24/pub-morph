2ac8348b5e62fc3007d925a5acb77a3a55602adb
vanzi <ivanl487@gmail.com>
2018-10-09 13:30:43 +0300

    new class added

---
+++ src/Morph.php
@@ -0,0 +1,23 @@
+<?php
+namespace MIR24\Morph;
+
+use MIR24\Morph\AbstractPubMorph;
+
+abstract class Morph extends AbstractPubMorph
+{
+    /*
+     * Search for DOM node inside pub text by attribute name and attribute value,
+     * removes node from publication, than returns publication text morphed.
+     * */ 
+    abstract public function removeIncut($nodeAttrValue) {
+        return 1;
+    };
+
+    /*
+     * Search for DOM node inside pub text by attribute name and attribute value,
+     * fillup node with a specific content, than returns publication text morphed.
+     * */ 
+    abstract public function placeIncut(int $nodeAttrValue, string $content) {
+        return 2;
+    }
+}
