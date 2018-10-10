6eb2bcfcb5fa10b79737f915fcb677e5403e1aae
vanzi <ivanl487@gmail.com>
2018-10-10 12:39:15 +0300

    logic cchange fix -

--- src/AbstractPubMorph.php
+++ src/AbstractPubMorph.php
@@ -13,9 +13,15 @@ use Sunra\PhpSimple\HtmlDomParser;
 abstract class AbstractPubMorph
 {
     protected $parser;
+    protected $decode;
     
-    function __construct($publicationSource) {
-        $this->parser = HtmlDomParser::str_get_html($publicationSource);
+    function __construct($publicationSource, $decode = true) {
+        $this->decode = $decode;
+        if ($decode) {
+            $this->parser = HtmlDomParser::str_get_html(htmlspecialchars_decode($publicationSource));
+        } else {
+            $this->parser = HtmlDomParser::str_get_html($publicationSource);
+        }
         return $this;
     }
 
@@ -23,17 +29,29 @@ abstract class AbstractPubMorph
         $this->parser->clear();
     }
 
-    abstract public function getHtmlString();
-    abstract public function getHtmlStringWithRegexEncode();
     /*
-     * Search for DOM node inside pub text by attribute name and attribute value,
-     * removes node from publication, than returns publication text morphed.
+     * Returns html string, dependig of decoding attribute
      * */ 
+    abstract public function getHtmlString();
+
+    /*
+     * Removes node from publication, than returns publication text morphed.
+     * */
     abstract public function removeIncut(int $incutId);
 
     /*
-     * Search for DOM node inside pub text by attribute name and attribute value,
-     * fillup node with a specific content, than returns publication text morphed.
+     * Fillup node with a specific content, than returns publication text morphed.
      * */ 
     abstract public function replaceIncut(int $incutId, string $content);
+
+    /*
+     * Fillup node with a specific content, making incut interactive, than 
+     * returns publication text morphed.
+     * */ 
+    public function makeIncutInactive(int $incutId, string $incutTitle);
+
+    /*
+     * Getting incut html tags ids attribute
+     * */ 
+    public function getIncutIds();
 }
--- src/Morph.php
+++ src/Morph.php
@@ -5,32 +5,43 @@ use MIR24\Morph\AbstractPubMorph;
 
 class Morph extends AbstractPubMorph
 {
-    public function getHtmlString() {
-        return $this->parser->save();
-    }
-    public function getHtmlStringWithRegexEncode() {
-        return preg_replace_callback(
-            config('morph-lib.regex.removeBrackets'),
-            function ($matches) {
-                return '{' . htmlspecialchars($matches[config('morph-lib.regex.preg_match_number')]) . '}';
-            },
-            $this->parser->save()
-        );
-    }
+
     /*
-     * Search for DOM node inside pub text by attribute name and attribute value,
-     * removes node from publication, than returns publication text morphed.
+     * Returns html string, dependig of decoding attribute
+     * */ 
+    public function getHtmlString() {
+        if ($this->decode) {
+            return $this->parser->save();
+        } else {
+            return preg_replace_callback(
+                config('morph-lib.regex.removeBrackets'),
+                function ($matches) {
+                    return '{' . htmlspecialchars($matches[config('morph-lib.regex.preg_match_number')]) . '}';
+                },
+                $this->parser->save()
+            );
+        }
+    }
+
+    /*
+     * Returns html string, with encoding, using regex
+     * */ 
+    public function getHtmlStringWithRegexEncode() {
+        
+    }
+
+    /*
+     * Removes node from publication, than returns publication text morphed.
      * */
     public function removeIncut(int $incutId) {
         foreach ($this->findIncutsById($incutId) as $node) {
-            $node->parent->outertext = '';
+            $node->outertext = '';
         }
         return $this;
     }
 
     /*
-     * Search for DOM node inside pub text by attribute name and attribute value,
-     * fillup node with a specific content, than returns publication text morphed.
+     * Fillup node with a specific content, than returns publication text morphed.
      * */ 
      public function replaceIncut(int $incutId, string $content) {
          foreach ($this->findIncutsById($incutId) as $node) {
@@ -38,7 +49,11 @@ class Morph extends AbstractPubMorph
          }
          return $this;
      }
-     
+
+     /*
+      * Fillup node with a specific content, making incut interactive, than 
+      * returns publication text morphed.
+      * */ 
      public function makeIncutInactive(int $incutId, string $incutTitle) {
          foreach ($this->findIncutsById($incutId) as $node) {
              $node->{config('morph-lib.incut.inactive.attr')} = config('morph-lib.incut.inactive.attrContent');
@@ -46,7 +61,10 @@ class Morph extends AbstractPubMorph
          }
          return $this;
      }
-     
+
+     /*
+      * Getting incut html tags ids attribute
+      * */ 
      public function getIncutIds() {
          $result = [];
          foreach ($this->findIncuts() as $node) {
@@ -54,11 +72,18 @@ class Morph extends AbstractPubMorph
          }
          return $result;
      }
-     
+
+     /*
+      * Search for DOM node inside pub text by attribute tag, attribute
+      * and attribute value
+      * */ 
      private function findIncutsById(int $incutId) {
          return $this->parser->find(config('morph-lib.incut.tag').'['. config('morph-lib.incut.attr').'='.$incutId.']');
      }
-     
+
+     /*
+      * Search for DOM node inside pub text by attribute tag and attribute value
+      * */ 
      private function findIncuts() {
          return $this->parser->find(config('morph-lib.incut.tag').'['. config('morph-lib.incut.attr').']');
      }
