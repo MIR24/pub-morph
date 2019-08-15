0dfa01bff8c66ed87a228f03f2a021f789bbcef5
vanzi <ivanl487@gmail.com>
2019-08-15 14:00:32 +0300

    incative text update

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -7,5 +7,6 @@ class Constants
     const EXCEPTION_MSG_START = 'Library MIR24\Morph ERROR:';
     const EXCEPTION_MSG_ROOT_NOT_NOT_FOUND = 'Root node not found';
     const EXCEPTION_MSG_INCUT_ATTR_NAME_NOT_FOUND = 'Incut attribute name doesn\'t exists';
+    const _TRANSLATION_FIELDS_FOR_ERROR = ', ПОЛЯ:';
 }
 ?>
