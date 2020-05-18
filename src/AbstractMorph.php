e319a7f7ef3e8fdba8a90e142b5b3b4460708c12
Обловацкий Юрий <jury@observat.by>
2020-05-18 16:37:37 +0300

    MIRSCR-1341-update-laravel. Use PHP 7.3 compatible php-simple-html-dom-parser

--- src/AbstractMorph.php
+++ src/AbstractMorph.php
@@ -1,19 +1,11 @@
 <?php
 namespace MIR24\Morph;
 
+use KubAT\PhpSimple\HtmlDomParser;
+use MIR24\Morph\Config\Constants;
+use MIR24\Morph\Helpers\BracketsHelper;
 use MIR24\Morph\Interfaces\DomParser\GetDomData;
 
-use Sunra\PhpSimple\HtmlDomParser;
-
-use MIR24\Morph\Config\Constants;
-
-use MIR24\Morph\Helpers\BracketsHelper;
-
-use MIR24\Morph\Components\Incut;
-use MIR24\Morph\Components\Banner;
-use MIR24\Morph\Components\IncutTemplateGenerator;
-use MIR24\Morph\Components\Amp;
-use MIR24\Morph\Components\Image;
 
 abstract class AbstractMorph implements GetDomData
 {
